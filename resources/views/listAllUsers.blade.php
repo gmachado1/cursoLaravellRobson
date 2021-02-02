2ec86e58d6cf2b8df3def9a0242940587a792266
gustavo.araujo_noesis <gustavo.araujo_noesis@prestador.globo>
2021-02-02 16:11:18 -0300

    colocando html pra ajudar

---
+++ resources/views/listAllUsers.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" 
+            content="width=device-width, user-scalable=no, inicial-scalable">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Listage de Usuários</title>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <td>#ID</td>
+                <td>Nome:</td>
+                <td>E-mail</td>
+                <td>Ações</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                    <a href="">Ver Usuários</a>
+                    <form action="" method="post">
+                        <input type="hidden" name="user" value="">
+                        <input type="submit" name="Remover">
+                    </form>
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
