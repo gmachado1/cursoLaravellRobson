a2f3cab12a5691564b8109077249b70fdcfbaac1
gustavo.araujo_noesis <gustavo.araujo_noesis@prestador.globo>
2021-02-03 18:06:39 -0300

    tesntado

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('usuarios', 'Form\\TestController')->names('user')->parameters(['usuarios'=>'user']);
+Route::resource('usuarios' , 'Form\\TestController')->names('user')->parameters(['usuarios'=>'user']);
